8edd3e836231cd5ef2e45e7b4811affe3cb5e97f
Sendoa82 <114100108+SendoaAldama@users.noreply.github.com>
2023-10-05 16:35:32 +0200

    Solucionamos el problema con las palas

--- Assets/Scripts/Palas/PalaMove1.cs
+++ Assets/Scripts/Palas/PalaMove1.cs
@@ -12,15 +12,23 @@ public class PalaMove1 : MonoBehaviour
         //Variable de posicion actual
     public Rigidbody2D rb2D;
 
+        //Recogemos donde esta
+    private float pisicionX;
+
+        //Recogemos valor
+    public new UnityEngine.Transform transform;
+
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        pisicionX = transform.position.x;   //Almacenamos donde se encuentra x
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if(Input.GetKey(KeyCode.A)) //Si pusla la A
         {
             rb2D.velocity = new Vector2(rb2D.velocity.x, speedMove);    //Vamos arriba
@@ -33,5 +41,9 @@ public class PalaMove1 : MonoBehaviour
         {
             rb2D.velocity = new Vector2(rb2D.velocity.x, 0f);    //Se queda en el sitio
         }
+
+        //Mantenemos las posicones y rotaciones para que no se gire y no se mueva del punto x
+        transform.position = new Vector2(pisicionX, rb2D.position.y);
+        transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
--- Assets/Scripts/Palas/PalaMove2.cs
+++ Assets/Scripts/Palas/PalaMove2.cs
@@ -6,32 +6,44 @@ public class PalaMove2 : MonoBehaviour
 {
 
     //Atributos
-        //Variable de movimiento arriba/abajo
+    //Variable de movimiento arriba/abajo
     public float speedMove = 5f;
 
-        //Variable de posicion actual
+    //Variable de posicion actual
     public Rigidbody2D rb2D;
+
+    //Recogemos donde esta
+    private float pisicionX;
+
+    //Recogemos valor
+    public new UnityEngine.Transform transform;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        pisicionX = transform.position.x;   //Almacenamos donde se encuentra x
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow)) //Si pulsamos la Flecha izquierda
+
+        if (Input.GetKey(KeyCode.A)) //Si pusla la A
         {
             rb2D.velocity = new Vector2(rb2D.velocity.x, speedMove);    //Vamos arriba
         }
-        else if (Input.GetKey(KeyCode.RightArrow)) //Si pulsamos la Flecha derecha
+        else if (Input.GetKey(KeyCode.D))   //Si pulsa la D
         {
-            rb2D.velocity = new Vector2(rb2D.velocity.x, -speedMove);    //Vamos abajo
+            rb2D.velocity = new Vector2(rb2D.velocity.x, -speedMove);   //Vamos abajo
         }
         else    //Sino
         {
-            rb2D.velocity = new Vector2(rb2D.velocity.x, 0);    //Se queda en el sitio
+            rb2D.velocity = new Vector2(rb2D.velocity.x, 0f);    //Se queda en el sitio
         }
+
+        //Mantenemos las posicones y rotaciones para que no se gire y no se mueva del punto x
+        transform.position = new Vector2(pisicionX, rb2D.position.y);
+        transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
